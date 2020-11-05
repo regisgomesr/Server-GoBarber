3c778bd50950471a1253674fd0baed86fb3ad2ee
Regis Ribeiro <regisgomesr@gmail.com>
2020-11-05 12:49:10 -0300

    Camada de Infra / Domínio

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import fs from 'fs';
 import uploadConfig from '../../../config/upload';
 
-import User from '../entities/User';
+import User from '../infra/typeorm/entities/User';
 import AppError from '../../../shared/errors/AppError';
 
 interface Request {
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,11 +4,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './shared/routes';
-import uploadConfig from './config/upload';
+import routes from './routes';
+import uploadConfig from '../../../config/upload';
 
-import './shared/database';
-import AppError from './shared/errors/AppError';
+import '../typeorm';
+import AppError from '../../errors/AppError';
 
 const app = express();
 
