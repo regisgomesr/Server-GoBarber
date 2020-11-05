a74d1b60d2bd6dfbdd243d6d9501ac7480234a4c
Regis Ribeiro <regisgomesr@gmail.com>
2020-11-05 19:11:14 -0300

    Aplicando principio (L) do SOLID - Liskov Substitution Principle em Appointments

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,10 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '@modules/appointments/repositories/AppointmentsRepository';
 import AppError from '@shared/errors/AppError';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 interface Request {
   provider_id: string;
