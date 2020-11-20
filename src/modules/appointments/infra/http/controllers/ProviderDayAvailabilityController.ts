613414d846edb44808fa8c4c7626a57dae56f641
Regis Ribeiro <regisgomesr@gmail.com>
2020-11-20 12:50:19 -0300

    Rotas e controllers

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderDayhAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderMonthAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { day, month, year } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayhAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(availability);
+  }
+}
