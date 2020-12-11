4d6ff98bfb3d5a9cb1723f5664f293b15276817d
Regis Ribeiro <regisgomesr@gmail.com>
2020-12-11 11:00:30 -0300

    Consertando bugs achados nos testes

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -49,8 +49,6 @@ class ListProviderMonthAvailabilityService {
         return getDate(appointment.date) === day;
       });
 
-      console.log(compareDate, new Date());
-
       return {
         day,
         available:
